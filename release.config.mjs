d210743d366101869e60d46a6b91ba9ad1477be4
Sergei S Betke <sergey.s.betke@yandex.ru>
2024-11-05 00:04:11 +0300

    ci: :construction_worker: add deployment workflow

--- release.config.mjs
+++ release.config.mjs
@@ -10,7 +10,13 @@ export default {
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		"@semantic-release/changelog",
-		"@semantic-release/git",
+		["@semantic-release/git", {
+			assets: [
+				"package.json",
+				"CHANGELOG.md"
+			],
+			message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+		}],
 		"@semantic-release/npm",
 		"@semantic-release/github"
 	]
