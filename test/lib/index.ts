51b22830bb85789067cb221350f6fdc4e4f3c89b
Sergei S Betke <sergey.s.betke@yandex.ru>
2024-11-10 15:57:29 +0300

    test: remove unexpected tests
    They are replaced by examples.
    
    Fixes #15

--- test/lib/index.ts
+++ test/lib/index.ts
@@ -1,11 +1,3 @@
 import path from "node:path";
-import { execSync } from "node:child_process";
 
-const rootPath: string = path.join(__dirname, '../..');
-
-export function gulpSync(gulpfile: string, task: string): Buffer {
-	return execSync(
-		`node --import tsx node_modules/gulp/bin/gulp --gulpfile "${path.resolve(__dirname, gulpfile)}" ${task}`,
-		{ cwd: rootPath }
-	);
-};
+export const rootPath: string = path.join(__dirname, '../..');
