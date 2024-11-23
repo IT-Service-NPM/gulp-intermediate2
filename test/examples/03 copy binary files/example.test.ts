8493c66fef0bc2bbb1b5e652912204a63d77cb97
Sergei S Betke <sergey.s.betke@yandex.ru>
2024-11-23 22:34:51 +0300

    test: add binary files copying example
    Fixes #15

---
+++ test/examples/03 copy binary files/example.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it, beforeEach, beforeAll, afterAll } from "vitest";
+import { promisify } from "node:util";
+import path from "node:path";
+import fs from "node:fs";
+import gulp from "gulp";
+import * as testLib from "../../lib";
+import "./gulpfile";
+
+const testSrcFilesPath: string = path.join(__dirname, 'test-files');
+const testDestFilesPath: string = path.join(__dirname, 'output');
+
+let testSrcFiles: string[];
+
+describe('intermediate2', () => {
+
+	beforeAll(async () => {
+		testSrcFiles = await testLib.getFilesRelativePath(testSrcFilesPath);
+	});
+
+	beforeEach(async () => {
+		await fs.promises.rm(testDestFilesPath, { force: true, recursive: true });
+	});
+
+	afterAll(async () => {
+		await fs.promises.rm(testDestFilesPath, { force: true, recursive: true });
+	});
+
+	it('must be copies all utf-8 and binary files with options', () => {
+		void (async () => {
+			/* eslint-disable-next-line @typescript-eslint/no-misused-promises */
+			await promisify(gulp.series('task1'))();
+
+			expect(fs.existsSync(testDestFilesPath), 'output dir must be exists').toBeTruthy();
+
+			const testDestFiles = await testLib.getFilesRelativePath(testDestFilesPath);
+
+			expect(testDestFiles).toEqual(testSrcFiles);
+
+			for (const testFilePath of testDestFiles) {
+				const srcContent = await fs.promises.readFile(path.join(testSrcFilesPath, testFilePath), { encoding: null });
+				const destContent = await fs.promises.readFile(path.join(testDestFilesPath, testFilePath), { encoding: null });
+				expect(destContent.equals(srcContent), `content of ${testFilePath} test file must be the same as content of source file`).toBeTruthy();
+			};
+		})();
+	});
+});
